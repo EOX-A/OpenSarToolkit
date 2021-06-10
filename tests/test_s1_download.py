52f3f86636a4d54f25c1346026c9936a71b571b7
Petr Ševčík <petr.sevcik@eox.at>
2021-06-10 10:07:50 +0200

    comment out download tests for now

--- tests/test_s1_download.py
+++ tests/test_s1_download.py
@@ -23,7 +23,7 @@ def test_asf_connection():
     assert response_code == control_code
 
 
-@pytest.mark.xfail(reason="Sometimes down for maintanance, allow to fail!!")
+# @pytest.mark.xfail(reason="Sometimes down for maintanance, allow to fail!!")
 def test_esa_scihub_connection(s1_grd_notnr_ost_product):
     herbert_uname = HERBERT_USER['uname']
     herbert_password = HERBERT_USER['pword']
@@ -42,51 +42,51 @@ def test_esa_scihub_connection(s1_grd_notnr_ost_product):
     assert uuid == control_uuid
 
 
-@pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
-                    reason="Skipping this test on Travis CI."
-                    )
-def test_asf_download(s1_grd_notnr_ost_product, mirror=2):
-    herbert_uname = HERBERT_USER['uname']
-    herbert_password = HERBERT_USER['asf_pword']
-    df = pd.DataFrame({'identifier': [s1_grd_notnr_ost_product[1].scene_id]})
-    with TemporaryDirectory(dir=os.getcwd()) as temp:
-        download_sentinel1(
-            inventory_df=df,
-            download_dir=temp,
-            mirror=mirror,
-            concurrent=1,
-            uname=herbert_uname,
-            pword=herbert_password
-        )
-        from ost.helpers.helpers import check_zipfile
-        product_path = s1_grd_notnr_ost_product[1].get_path(
-            download_dir=temp,
-        )
-        return_code = check_zipfile(product_path)
-        assert return_code is None
-
-
-@pytest.mark.xfail(reason="This is currently not usually working, so allow to fail!!")
-def test_esa_scihub_download(s1_mai_2021_id,
-                             mirror=1
-                             ):
-    herbert_uname = HERBERT_USER['uname']
-    herbert_password = HERBERT_USER['pword']
-    product = S1Scene(s1_mai_2021_id)
-    df = pd.DataFrame({'identifier': [product.scene_id]})
-    with TemporaryDirectory(dir=os.getcwd()) as temp:
-        download_sentinel1(
-            inventory_df=df,
-            download_dir=temp,
-            mirror=mirror,
-            concurrent=1,
-            uname=herbert_uname,
-            pword=herbert_password
-        )
-
-        product_path = product.get_path(
-            download_dir=temp,
-            data_mount='/eodata'
-        )
-        return_code = check_zipfile(product_path)
-        assert return_code is None
+# @pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+#                     reason="Skipping this test on Travis CI."
+#                     )
+# def test_asf_download(s1_grd_notnr_ost_product, mirror=2):
+#     herbert_uname = HERBERT_USER['uname']
+#     herbert_password = HERBERT_USER['asf_pword']
+#     df = pd.DataFrame({'identifier': [s1_grd_notnr_ost_product[1].scene_id]})
+#     with TemporaryDirectory(dir=os.getcwd()) as temp:
+#         download_sentinel1(
+#             inventory_df=df,
+#             download_dir=temp,
+#             mirror=mirror,
+#             concurrent=1,
+#             uname=herbert_uname,
+#             pword=herbert_password
+#         )
+#         from ost.helpers.helpers import check_zipfile
+#         product_path = s1_grd_notnr_ost_product[1].get_path(
+#             download_dir=temp,
+#         )
+#         return_code = check_zipfile(product_path)
+#         assert return_code is None
+#
+#
+# @pytest.mark.xfail(reason="This is currently not usually working, so allow to fail!!")
+# def test_esa_scihub_download(s1_mai_2021_id,
+#                              mirror=1
+#                              ):
+#     herbert_uname = HERBERT_USER['uname']
+#     herbert_password = HERBERT_USER['pword']
+#     product = S1Scene(s1_mai_2021_id)
+#     df = pd.DataFrame({'identifier': [product.scene_id]})
+#     with TemporaryDirectory(dir=os.getcwd()) as temp:
+#         download_sentinel1(
+#             inventory_df=df,
+#             download_dir=temp,
+#             mirror=mirror,
+#             concurrent=1,
+#             uname=herbert_uname,
+#             pword=herbert_password
+#         )
+#
+#         product_path = product.get_path(
+#             download_dir=temp,
+#             data_mount='/eodata'
+#         )
+#         return_code = check_zipfile(product_path)
+#         assert return_code is None
